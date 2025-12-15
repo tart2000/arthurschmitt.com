ad8a540f24f1f29050386a3ffaf79ac05961a687
arthur “tart2000” schmitt (via WeWeb) <tart2000design@gmail.com>
2025-12-15 12:40:09 +0100

    v27 - better mobile

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 26;
+const version = 27;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
